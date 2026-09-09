fc6c425e8efd684fb738af739b04bf30cb12b5c1
leonardo <leonardo.valencia@gaisler.com>
2026-09-09 20:43:54 +0200

    mistake

--- tests/test4_hierarchical/outputs/hierarchical_test4.dc_floorplan.tcl
+++
@@ -1,5 +0,0 @@
-create_die_area -coordinate [ list  0 0  300 300 ]
-set_cell_location ram_hier/itag/itag_ram_0 -coordinates [ list 100 130 ] -fixed -orientation N
-set_cell_location ram_hier/itag/itag_ram_1 -coordinates [ list 120 130 ] -fixed -orientation N
-set_cell_location ram_hier/idata/idata_ram_h -coordinates [ list 100 160 ] -fixed -orientation N
-set_cell_location ram_hier/idata/idata_ram_l -coordinates [ list 120 160 ] -fixed -orientation N
